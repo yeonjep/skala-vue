0b65d643cca5161cf9a75f9a50501adcce1bb700
Yeonje Park <yeonje1004@naver.com>
2026-08-11 14:10:34 +0900

    Weather Mockup 실습

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueDevTools()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  // 🟢 [커스텀 추가 1] 로컬 개발 서버(Dev Server) 속성 제어
+  server: {
+    port: 3000, // 개발 서버의 네트워크 포트를 3000번으로 고정 명세
+    open: true, // 프로세스 기동(npm run dev) 시 기본 웹 브라우저를 자동 실행
+  },
+  // 🟢 [커스텀 추가 2] 컴파일 완료된 산출물(Production Build) 사양 제어
+  build: {
+    outDir: 'dist', // 최종 정적 리소스(HTML, JS, CSS)가 저장될 출력 디렉토리명 지정
+  },
+})
